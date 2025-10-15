b2e4ad7b82d8c58048caaf6615f82fdc0f05fad2
Phạm Vinh <ilai>
2025-10-15 13:07:04 +0700

    fix: relative import paths for Railway

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.mode ==="development" ? "http://localhost:5000/api" : 
-    "https://mern-ecommerce-be-production.up.railway.app/api",
+    baseURL: "https://mern-ecommerce-be-production.up.railway.app/api",
     withCredentials:true, // send cookies to the server
 })
 
